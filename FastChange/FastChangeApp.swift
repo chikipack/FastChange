fc9c4d538b6e90d4b63b03bbc73c14fa87b3d51e
VictorAnayaCMX <victor.anaya@caliente.mx>
2025-07-07 09:42:53 -0700

    Initial Commit

---
+++ FastChange/FastChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastChangeApp.swift
+//  FastChange
+//
+//  Created by Victor Alejandro Anaya Martinez on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FastChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
